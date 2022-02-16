0ec50a78aae152ac5f9583d1a3d553c44365e117
Priyanshu Mishra <priyanshu12651265@gmail.com>
2022-02-16 19:18:39 +0530

    Alpha

---
+++ home.js
@@ -0,0 +1,18 @@
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+$(document).ready(function(){
+  $(".toggle").click(function(){
+    $(".item.first").slideToggle(100);
+    $(".item.second").slideToggle(200);
+    $(".item.third").slideToggle(300);
+    $(".item.last").slideToggle(400);
+    if (w >= 600){
+      $(".item.button").css("display","block");
+      $(".item.secondary").css("display","block");
+    }else{
+    $(".item.button").slideToggle(500);
+    $(".item.secondary").slideToggle(600);
+  }
+  });
+});
