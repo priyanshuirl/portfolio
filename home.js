f3dbe36e252b17fb4f90b9143faf574441263bf9
Priyanshu Mishra <priyanshu12651265@gmail.com>
2022-07-15 14:44:34 +0530

    Fixed navbar click issue

--- home.js
+++ home.js
@@ -1,18 +1,33 @@
 var w = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-$(document).ready(function(){
-  $(".toggle").click(function(){
+  || document.documentElement.clientWidth
+  || document.body.clientWidth;
+$(document).ready(function () {
+  $(".toggle").click(function () {
     $(".item.first").slideToggle(100);
     $(".item.second").slideToggle(200);
     $(".item.third").slideToggle(300);
     $(".item.last").slideToggle(400);
-    if (w >= 600){
-      $(".item.button").css("display","block");
-      $(".item.secondary").css("display","block");
-    }else{
-    $(".item.button").slideToggle(500);
-    $(".item.secondary").slideToggle(600);
-  }
+    if (w >= 600) {
+      $(".item.button").css("display", "block");
+      $(".item.secondary").css("display", "block");
+    } else {
+      $(".item.button").slideToggle(500);
+      $(".item.secondary").slideToggle(600);
+    }
+  });
+  $(".item").click(function () {
+    if (window.innerWidth < 590) {
+      $(".item.first").slideToggle(100);
+      $(".item.second").slideToggle(200);
+      $(".item.third").slideToggle(300);
+      $(".item.last").slideToggle(400);
+      if (w >= 600) {
+        $(".item.button").css("display", "block");
+        $(".item.secondary").css("display", "block");
+      } else {
+        $(".item.button").slideToggle(500);
+        $(".item.secondary").slideToggle(600);
+      }
+    }
   });
 });
